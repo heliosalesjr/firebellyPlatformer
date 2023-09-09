2a1e12b2fbdeae7e6844aa9e4a5a65b795ab2bc4
Helio Sales Jr <heliosalesjr@gmail.com>
2023-09-08 21:19:45 -0300

    quick movement fixes

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -150.0
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 300
@@ -15,15 +16,16 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	
 	move_and_slide()
